8d256bdc4ed1f55fb85b77510970bce35b7c8ba6
thedavidemmanuele <d.emmanuel@alustudent.com>
2023-03-30 03:31:09 +0000

    api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
+
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    completed_tasks = [task for task in todos if task.get("completed")]
+
+    print("Employee {} is done with tasks({}/{})".format(employee_name, len(completed_tasks), total_tasks))
+    print("Completed tasks:")
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
+
