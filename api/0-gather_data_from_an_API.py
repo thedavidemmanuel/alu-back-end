198d2a341bedba1466bea49653a34d7054d210fa
thedavidemmanuele <d.emmanuel@alustudent.com>
2023-03-30 04:25:14 +0000

    updates

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    python script that returns TODO list progress for a given employee ID
+Python script that returns TODO list progress for a given employee ID
 """
 import json
 import requests
@@ -9,40 +9,40 @@ from sys import argv
 
 if __name__ == "__main__":
     """
-        request user info by employee ID
+    Request user info by employee ID
     """
     request_employee = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
     """
-        convert json to dictionary
+    Convert JSON to dictionary
     """
     employee = json.loads(request_employee.text)
     """
-        extract employee name
+    Extract employee name
     """
     employee_name = employee.get("name")
 
     """
-        request user's TODO list
+    Request user's TODO list
     """
     request_todos = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
     """
-        dictionary to store task status in boolean format
+    Dictionary to store task status in boolean format
     """
     tasks = {}
     """
-        convert json to list of dictionaries
+    Convert JSON to list of dictionaries
     """
     employee_todos = json.loads(request_todos.text)
     """
-        loop through dictionary & get completed tasks
+    Loop through dictionary & get completed tasks
     """
     for dictionary in employee_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
     """
-        return name, total number of tasks & completed tasks
+    Return name, total number of tasks & completed tasks
     """
     EMPLOYEE_NAME = employee_name
     TOTAL_NUMBER_OF_TASKS = len(tasks)
@@ -51,5 +51,5 @@ if __name__ == "__main__":
         EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for k, v in tasks.items():
         if v is True:
-            print("\t {}".format(k))
+            print("\t{}".format(k))
 
